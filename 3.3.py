0ea0c12a50f8f10c283d7f5597a4ec6711f7b84c
miguel-cumming <57871774+miguel-cumming@users.noreply.github.com>
2019-12-08 18:12:32 -0500

    Add files via upload

---
+++ 3.3.py
@@ -0,0 +1,32 @@
+#I think my solution is more elegant than the offical one http://greenteapress.com/thinkpython2/code/grid.py
+#In particular, the official solution exploits vacuous functions wheras I exploit vacuous 'arguments'
+
+def call_two(function, value1, value2):
+    function(value1, value2)
+    function(value1, value2)
+
+def call_four(function, value1, value2):
+    call_two(function, value1, value2)
+    call_two(function, value1, value2)
+
+
+fragment1 = '+' + ('-' * 4)
+fragment2 = '|' + (' ' * 4)
+
+line1 = fragment1 * 2 + '+'
+line2 = fragment2 * 2 + '|'
+
+def block(line1, line2):
+    print(line1)
+    call_four(print, line2, '')
+
+call_two(block, line1, line2)
+print(line1)
+
+#pt.2
+new_line1 = fragment1 * 4 + '+'
+new_line2 = fragment2 * 4 + '|'
+
+call_four(block, new_line1, new_line2)
+print(new_line1)
+
